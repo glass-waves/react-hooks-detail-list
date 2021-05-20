d19b0c037ea9988b9c6c8727b1d6b23c69c7078d
Dylan White <dylan.whitej@gmail.com>
2021-05-20 11:12:33 -0700

    fixed warnings

--- src/services/fetchCards.js
+++ src/services/fetchCards.js
@@ -2,14 +2,14 @@ const fetchCards = async () => {
     const res = await fetch('https://api.magicthegathering.io/v1/cards');
     const { cards } = await res.json();
     return cards.map((card) => ({
-        name: card.name,
-        image: card.imageUrl,
-        cardType: card.type,
-        rarity: card.rarity,
-        text: card.text,
-        power: card.power,
-        toughness: card.toughness,
-        id: card.id,
+        name: card.name || 'NA',
+        image: card.imageUrl || 'NA',
+        cardType: card.type || 'NA',
+        rarity: card.rarity || 'NA',
+        text: card.text || 'NA',
+        power: card.power || 'NA',
+        toughness: card.toughness || 'NA',
+        id: card.id || 'NA',
     }));
 }
 
--- src/services/fetchSingleCard.js
+++ src/services/fetchSingleCard.js
@@ -2,14 +2,14 @@ export const fetchSingleCard = async(id) => {
     const res = await fetch(`https://api.magicthegathering.io/v1/cards/${id}`)
     const { card } = await res.json();
     return {
-        name: card.name,
-        image: card.imageUrl,
-        cardType: card.type,
-        rarity: card.rarity,
-        text: card.text,
-        power: card.power,
-        toughness: card.toughness,
-        id: card.id,
+        name: card.name || 'NA',
+        image: card.imageUrl || 'NA',
+        cardType: card.type || 'NA',
+        rarity: card.rarity || 'NA',
+        text: card.text || 'NA',
+        power: card.power || 'NA',
+        toughness: card.toughness || 'NA',
+        id: card.id || 'NA',
     };
 
 } 
